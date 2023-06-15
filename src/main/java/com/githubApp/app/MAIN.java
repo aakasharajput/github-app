5846ed5581235ee806b856eb903b6cfc68ca166e
Aakash Rajput <aakash.rajput@techverito.com>
2023-06-15 17:42:44 +0530

    Initial commit

---
+++ src/main/java/com/githubApp/app/MAIN.java
@@ -0,0 +1,13 @@
+package com.githubApp.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MAIN {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MAIN.class, args);
+	}
+
+}
---
+++ src/test/java/com/githubApp/app/TEST.java
@@ -0,0 +1,13 @@
+package com.githubApp.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TEST {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
